b15e219680fe0793c174991364a75be8b0811953
Luc Pezet <lpezet@gmail.com>
2013-06-14 15:22:59 +0000

    log added

--- gwt-videoplayer-core/src/main/java/net/sf/video4j/gwt/client/VideoPlayerDemo.java
+++ gwt-videoplayer-core/src/main/java/net/sf/video4j/gwt/client/VideoPlayerDemo.java
@@ -5,12 +5,18 @@ package net.sf.video4j.gwt.client;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import fr.hd3d.html5.video.client.VideoSource;
 import fr.hd3d.html5.video.client.VideoSource.VideoType;
+import fr.hd3d.html5.video.client.events.VideoPlayingEvent;
+import fr.hd3d.html5.video.client.events.VideoTimeUpdateEvent;
+import fr.hd3d.html5.video.client.handlers.VideoPlayingHandler;
+import fr.hd3d.html5.video.client.handlers.VideoTimeUpdateHandler;
 import fr.hd3d.html5.video.client.VideoWidget;
 
 /**
@@ -18,6 +24,8 @@ import fr.hd3d.html5.video.client.VideoWidget;
  *
  */
 public class VideoPlayerDemo implements EntryPoint {
+	
+	private Logger mLogger = Logger.getLogger(VideoPlayerDemo.class.getName());
 
 	@Override
 	public void onModuleLoad() {
@@ -27,6 +35,21 @@ public class VideoPlayerDemo implements EntryPoint {
         //sources.add(new VideoSource("fr_hd3d_html5_video_demo/videos/bbb_trailer_360p.webm", VideoType.WEBM));
         videoPlayer.setSources(sources);
         videoPlayer.setPixelSize(500, 400);
+        videoPlayer.addPlayingHandler(new VideoPlayingHandler() {
+			
+			@Override
+			public void onPlaying(VideoPlayingEvent event) {
+				mLogger.log(Level.SEVERE, "Playing!!!");
+			}
+		});
+        videoPlayer.setPoster("http://i.tripfilmsws.com/t/500x400/tripfilms_snapshots/720p/D93A130B1BC3E02EB7AB99812EFB8C00_0002.jpg");
+        videoPlayer.addTimeUpdateHandler(new VideoTimeUpdateHandler() {
+			
+			@Override
+			public void onTimeUpdated(VideoTimeUpdateEvent event) {
+				mLogger.log(Level.INFO, "Time update!" + event.getCurrentTime());
+			}
+		});
         RootPanel.get().add(videoPlayer);
 	}
 }
