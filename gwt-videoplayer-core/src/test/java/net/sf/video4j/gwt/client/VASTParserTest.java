2dc76fc04dd325ca6bdbd3e3c441ffaa2fde7f5c
Luc Pezet <lpezet@gmail.com>
2013-07-09 04:57:31 +0000

    now testing regular linera

--- gwt-videoplayer-core/src/test/java/net/sf/video4j/gwt/client/VASTParserTest.java
+++ gwt-videoplayer-core/src/test/java/net/sf/video4j/gwt/client/VASTParserTest.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 
+import net.sf.video4j.gwt.client.be.Ad;
+import net.sf.video4j.gwt.client.be.InLine;
 import net.sf.video4j.gwt.client.be.VAST;
 
 import org.apache.commons.io.IOUtils;
@@ -23,17 +25,37 @@ import com.googlecode.gwt.test.GwtTest;
 public class VASTParserTest extends GwtTest {
 
 	@Test
-	public void shouldParse() throws Exception {
+	public void overlays() throws Exception {
 		VASTParser oParser = new  VASTParser();
-		String oXML = getXMLAsString();
+		String oXML = getXMLAsString("/vast2_overlays.xml");
 		
 		VAST oVAST = oParser.parse(oXML);
 		
 		assertNotNull(oVAST);
 	}
+	
+	@Test
+	public void regularLinear() throws Exception {
+		VASTParser oParser = new  VASTParser();
+		String oXML = getXMLAsString("/vast2_regular_linear.xml");
+		
+		VAST oVAST = oParser.parse(oXML);
+		assertNotNull(oVAST);
+		assertNotNull(oVAST.getAds());
+		assertEquals(1, oVAST.getAds().size());
+		
+		Ad oAd = oVAST.getAds().get(0);
+		assertEquals("preroll-1", oAd.getId());
+		assertTrue(oAd instanceof InLine);
+		
+		InLine oInLine = (InLine) oAd;
+		assertEquals("2.0", oInLine.getAdSystem().getName());
+		assertEquals("5748406", oInLine.getAdTitle());
+		assertEquals(2, oInLine.getCreatives().size());
+	}
 
-	private String getXMLAsString() throws IOException {
-		InputStream oIS = this.getClass().getResourceAsStream("/vast2_overlays.xml");
+	private String getXMLAsString(String pResourcePath) throws IOException {
+		InputStream oIS = this.getClass().getResourceAsStream(pResourcePath);
 		StringWriter writer = new StringWriter();
 		IOUtils.copy(oIS, writer, "UTF8");
 		String oXML = writer.toString();
