b16fe37c2afc417e5286b9890df82730d25cc420
Luc Pezet <lpezet@gmail.com>
2013-08-14 06:53:12 +0000

    methods added

--- gwt-videoplayer-core/src/main/java/net/sf/video4j/gwt/client/handler/PlayerUiHandlers.java
+++ gwt-videoplayer-core/src/main/java/net/sf/video4j/gwt/client/handler/PlayerUiHandlers.java
@@ -1,5 +1,7 @@
 package net.sf.video4j.gwt.client.handler;
 
+import net.sf.video4j.gwt.client.player.PlayItem;
+
 import com.gwtplatform.mvp.client.UiHandlers;
 
 /**
@@ -8,4 +10,9 @@ import com.gwtplatform.mvp.client.UiHandlers;
 public interface PlayerUiHandlers extends UiHandlers {
 
 	void onTimeUpdate(double pCurrentTime);
+	void onError();
+	void onPlaying();
+	void onPause();
+	void onEnded();
+	void onDurationChanged(double pNewDuration);
 }
