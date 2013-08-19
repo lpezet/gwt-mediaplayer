7668aa35388dcec932fbfd0f85e519c80aee110a
Luc Pezet <lpezet@gmail.com>
2013-08-19 05:56:11 +0000

    fixed

--- gwt-videoplayer-core/src/test/java/net/sf/video4j/gwt/client/player/CuePointManagerTest.java
+++ gwt-videoplayer-core/src/test/java/net/sf/video4j/gwt/client/player/CuePointManagerTest.java
@@ -5,24 +5,15 @@ package net.sf.video4j.gwt.client.player;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import net.sf.video4j.gwt.client.event.CuePointEvent;
+import net.sf.video4j.gwt.client.event.PlayerPlayingEvent;
+
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
-import com.google.gwt.event.shared.GwtEvent;
-
-import fr.hd3d.html5.video.client.IVideoPlayer;
-import fr.hd3d.html5.video.client.events.VideoCuePointEvent;
-import fr.hd3d.html5.video.client.events.VideoTimeUpdateEvent;
-import fr.hd3d.html5.video.client.handlers.VideoTimeUpdateHandler;
+import com.google.web.bindery.event.shared.testing.CountingEventBus;
 
 /**
  * @author luc
@@ -32,6 +23,7 @@ public class CuePointManagerTest {
 
     @Test
     public void doIt() throws Exception {
+    	/*
         IVideoPlayer oPlayer = mock(IVideoPlayer.class);
         when(oPlayer.addTimeUpdateHandler(any(VideoTimeUpdateHandler.class))).thenReturn(null);
 
@@ -42,8 +34,8 @@ public class CuePointManagerTest {
             @Override
             public Object answer(InvocationOnMock pInvocation) throws Throwable {
                 GwtEvent<?> oEvent = (GwtEvent<?>) pInvocation.getArguments()[0];
-                if (oEvent instanceof VideoCuePointEvent) {
-                    VideoCuePointEvent oCuePointEvent = (VideoCuePointEvent) oEvent;
+                if (oEvent instanceof CuePointEvent) {
+                    CuePointEvent oCuePointEvent = (CuePointEvent) oEvent;
                     CuePoint oCP = oCuePointEvent.getCuePoint();
                     System.out.println("Got CuePoint: " + oCP.toString());
                 } else {
@@ -52,8 +44,9 @@ public class CuePointManagerTest {
                 return null;
             }
         }).when(oPlayer).fireEvent(anyObject());
-
-        CuePointManager oManager = new CuePointManager(oPlayer);
+		*/
+    	CountingEventBus oBus = new CountingEventBus();
+        CuePointManager oManager = new CuePointManager(oBus);
 
         CuePoint oActual = oManager.add(1000, "A", null);
         assertNotNull(oActual);
@@ -66,24 +59,24 @@ public class CuePointManagerTest {
         assertNotNull(oActual);
         assertEquals(2, oActualList.size());
         assertEquals("B", oActualList.get(1).getName());
-
-        VideoTimeUpdateEvent oEvent = new VideoTimeUpdateEvent();
-        oEvent.setCurrentTime(0);
-        oManager.onTimeUpdated(oEvent);
-
-        oEvent.setCurrentTime(0.998);
-        oManager.onTimeUpdated(oEvent);
-
-        oEvent.setCurrentTime(1.002);
-        oManager.onTimeUpdated(oEvent);
-
-        oEvent.setCurrentTime(1.997);
-        oManager.onTimeUpdated(oEvent);
-
-        oEvent.setCurrentTime(1.999);
-        oManager.onTimeUpdated(oEvent);
-
-        oEvent.setCurrentTime(19.999);
-        oManager.onTimeUpdated(oEvent);
+        
+        oBus.fireEvent(new PlayerPlayingEvent(0));
+        assertEquals(0, oBus.getFiredCount(CuePointEvent.getType()));
+        
+        oBus.fireEvent(new PlayerPlayingEvent(0.997));
+        assertEquals(0, oBus.getFiredCount(CuePointEvent.getType()));
+        
+        oBus.fireEvent(new PlayerPlayingEvent(1.002));
+        assertEquals(1, oBus.getFiredCount(CuePointEvent.getType()));
+        
+        oBus.fireEvent(new PlayerPlayingEvent(1.997));
+        assertEquals(1, oBus.getFiredCount(CuePointEvent.getType()));
+        
+        oBus.fireEvent(new PlayerPlayingEvent(1.999));
+        assertEquals(2, oBus.getFiredCount(CuePointEvent.getType()));
+        
+        oBus.fireEvent(new PlayerPlayingEvent(19.999));
+        assertEquals(2, oBus.getFiredCount(CuePointEvent.getType()));
+        
     }
 }
