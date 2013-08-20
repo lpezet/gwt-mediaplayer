a24872e3e89aa1d609b20043c6940c15c429f931
Luc Pezet <lpezet@gmail.com>
2013-08-20 04:45:41 +0000

    updated

--- gwt-videoplayer-demo/src/main/java/net/sf/video4j/gwt/client/demo/VideoPlayerCuePointDemo.java
+++ gwt-videoplayer-demo/src/main/java/net/sf/video4j/gwt/client/demo/VideoPlayerCuePointDemo.java
@@ -5,8 +5,9 @@ package net.sf.video4j.gwt.client.demo;
 
 import java.util.logging.Logger;
 
-import net.sf.video4j.gwt.client.demo.VideoPlayerCuePointDemo;
-import net.sf.video4j.gwt.client.demo.VideoPlayerOverlayDemo;
+import net.sf.video4j.gwt.client.event.CuePointEvent;
+import net.sf.video4j.gwt.client.event.CuePointEvent.CuePointHandler;
+import net.sf.video4j.gwt.client.event.PlayerPlayingEvent;
 import net.sf.video4j.gwt.client.player.CuePointManager;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -17,13 +18,13 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
 import fr.hd3d.html5.video.client.VideoSource;
 import fr.hd3d.html5.video.client.VideoSource.VideoType;
 import fr.hd3d.html5.video.client.VideoWidget;
-import fr.hd3d.html5.video.client.events.VideoCuePointEvent;
-import fr.hd3d.html5.video.client.handlers.VideoCuePointHandler;
+import fr.hd3d.html5.video.client.events.VideoTimeUpdateEvent;
+import fr.hd3d.html5.video.client.handlers.VideoTimeUpdateHandler;
 
 /**
  * @author luc
@@ -31,7 +32,7 @@ import fr.hd3d.html5.video.client.handlers.VideoCuePointHandler;
  */
 public class VideoPlayerCuePointDemo implements EntryPoint {
 	
-	private Logger mLogger = Logger.getLogger(VideoPlayerOverlayDemo.class.getName());
+	private Logger mLogger = Logger.getLogger(VideoPlayerCuePointDemo.class.getName());
 
 	private static final String OVERLAY = "http://openx.openvideoads.org/openx-2.8.2/www/images/fa5b35e2e16d4b2a922e4169c9fcea97.gif";
 	private static final String VIDEO = "http://videos.tripfilms.com/720p/0D933B4160C30D1A18CCFA2AB86EDE23.mp4";
@@ -45,6 +46,7 @@ public class VideoPlayerCuePointDemo implements EntryPoint {
 	
 	@Override
 	public void onModuleLoad() {
+		final SimpleEventBus oBus = new SimpleEventBus();
 		HTMLPanel outer = binder.createAndBindUi(this);
         RootPanel.get("player").add(outer);
 		//RootPanel.get().add(container);
@@ -56,21 +58,29 @@ public class VideoPlayerCuePointDemo implements EntryPoint {
         videoPlayer.addSource(new VideoSource(VIDEO, VideoType.MP4));
         videoPlayer.setPixelSize(500, 400);
         
-        CuePointManager oCPM = new CuePointManager(videoPlayer);
+        CuePointManager oCPM = new CuePointManager(oBus);
         oCPM.add(2000, "show_overlay", null);
         oCPM.add(10000, "hide_overlay", null);
         
-        VideoCuePointHandler oCPHandler = new VideoCuePointHandler() {
-        	public void onCuePoint(VideoCuePointEvent pEvent) {
-        		if ("show_overlay".equalsIgnoreCase(pEvent.getCuePoint().getName())) {
+        CuePointHandler oCPHandler = new CuePointHandler() {
+			@Override
+			public void onCuePoint(CuePointEvent pEvent) {
+				if ("show_overlay".equalsIgnoreCase(pEvent.getCuePoint().getName())) {
         			Image i = new Image(OVERLAY);
 					overlayContainer.add(i);
         		} else {
         			overlayContainer.clear();
         		}
-        	};
-        };
-        videoPlayer.addCuePointHandler(oCPHandler);
+			}
+		};
+		oBus.addHandler(CuePointEvent.getType(), oCPHandler);
+		
+		videoPlayer.addTimeUpdateHandler(new VideoTimeUpdateHandler() {
+			@Override
+			public void onTimeUpdated(VideoTimeUpdateEvent pEvent) {
+				oBus.fireEvent(new PlayerPlayingEvent(pEvent.getCurrentTime()));
+			}
+		});
         
         videoContainer.add(videoPlayer);    
         
