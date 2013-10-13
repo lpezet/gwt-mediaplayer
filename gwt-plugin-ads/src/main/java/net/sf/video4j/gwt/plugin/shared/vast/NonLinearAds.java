d1fc2ee499e0b3be0e6dca69a5a2de355022ad3a
Luc Pezet <lpezet@gmail.com>
2013-10-13 07:36:56 +0000

    ads added

--- gwt-plugin-ads/src/main/java/net/sf/video4j/gwt/plugin/shared/vast/NonLinearAds.java
+++ gwt-plugin-ads/src/main/java/net/sf/video4j/gwt/plugin/shared/vast/NonLinearAds.java
@@ -13,12 +13,26 @@ import java.util.List;
 public class NonLinearAds extends Creative {
 
     private List<Tracking> mTrackingEvents = new ArrayList<Tracking>();
-
-    @Override
-    public String toString() {
-        return "NonLinearAds [mTrackingEvents=" + mTrackingEvents + "]";
-    }
-
+    private List<NonLinearAd> mList = new ArrayList<NonLinearAd>();
+    
     // Any number of companions in any desired pixel dimensions.
+
+	public List<NonLinearAd> getList() {
+		return mList;
+	}
+	public void setList(List<NonLinearAd> pList) {
+		mList = pList;
+	}
+	public List<Tracking> getTrackingEvents() {
+		return mTrackingEvents;
+	}
+	public void setTrackingEvents(List<Tracking> pTrackingEvents) {
+		mTrackingEvents = pTrackingEvents;
+	}
+	
+	@Override
+    public String toString() {
+        return "NonLinearAds [ads=" + mList + ", trackingEvents=" + mTrackingEvents + "]";
+    }
 
 }
