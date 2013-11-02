6e09baa60f6a339ae5b3b95c39a35e47ec850810
Luc Pezet <lpezet@gmail.com>
2013-11-02 23:49:31 +0000

    getPlaylist() implemented

--- gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/model/ApplicationConfig.java
+++ gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/model/ApplicationConfig.java
@@ -3,19 +3,16 @@
  */
 package net.sf.video4j.gwt.client.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-
-import net.sf.video4j.gwt.client.config.model.jso.JSOApplicationConfig;
-import net.sf.video4j.gwt.client.player.Playlist;
 
 /**
  * @author luc
  *
  */
 public class ApplicationConfig implements IApplicationConfig {
+	
+	private JSONArray mPlaylist;
 
 	@Override
 	public JSONObject getCommon() {
@@ -24,15 +21,18 @@ public class ApplicationConfig implements IApplicationConfig {
 	}
 
 	@Override
-	public JSONObject getPlaylist() {
-		// TODO Auto-generated method stub
-		return null;
+	public JSONArray getPlaylist() {
+		return mPlaylist;
 	}
 
 	@Override
 	public JSONObject getPlugins() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void setPlaylist(JSONArray pJSON) {
+		mPlaylist = pJSON;
 	}
 
 }
