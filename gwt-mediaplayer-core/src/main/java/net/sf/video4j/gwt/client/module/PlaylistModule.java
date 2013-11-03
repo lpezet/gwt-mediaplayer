5770e81ab4ea42796d7d9202bc8616a059272926
Luc Pezet <lpezet@gmail.com>
2013-11-03 23:49:46 +0000

    binding PlayItemBeanFactory

--- gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/module/PlaylistModule.java
+++ gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/module/PlaylistModule.java
@@ -4,6 +4,7 @@
 package net.sf.video4j.gwt.client.module;
 
 import net.sf.video4j.gwt.client.controller.PlaylistController;
+import net.sf.video4j.gwt.client.util.PlayItemBeanFactory;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 
@@ -16,6 +17,7 @@ public class PlaylistModule extends AbstractGinModule {
 	@Override
 	protected void configure() {
 		bind(PlaylistController.class);
+		bind(PlayItemBeanFactory.class);
 	}
 
 }
