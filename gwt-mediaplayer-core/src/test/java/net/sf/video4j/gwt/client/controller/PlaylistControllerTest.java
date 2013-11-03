f7f0f21a2ae7479711e240a23319b2553d9c058a
Luc Pezet <lpezet@gmail.com>
2013-11-03 02:39:05 +0000

    test fixed

--- gwt-mediaplayer-core/src/test/java/net/sf/video4j/gwt/client/controller/PlaylistControllerTest.java
+++ gwt-mediaplayer-core/src/test/java/net/sf/video4j/gwt/client/controller/PlaylistControllerTest.java
@@ -4,7 +4,6 @@
 package net.sf.video4j.gwt.client.controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import net.sf.video4j.gwt.client.event.ApplicationLoadEvent;
 import net.sf.video4j.gwt.client.event.ApplicationReadyEvent;
 import net.sf.video4j.gwt.client.event.PlayerPlayEndedEvent;
@@ -15,58 +14,58 @@ import net.sf.video4j.gwt.client.util.PlayItemBeanFactory;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.testing.CountingEventBus;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
+import com.googlecode.gwt.test.GwtModule;
+import com.googlecode.gwt.test.GwtTest;
 
 /**
  * @author luc
  *
  */
-@RunWith(MockitoJUnitRunner.class)
-public class PlaylistControllerTest {
+@GwtModule("net.sf.video4j.gwt.client.controller.PlaylistController")
+public class PlaylistControllerTest extends GwtTest {
 
 	private PlaylistController mPlaylistController;
 	
-	private CountingEventBus mEventBus = new CountingEventBus();
+	private CountingEventBus mEventBus;
 	
-	@Mock
 	private PlayItemBeanFactory mPlayItemBeanFactory;
 	
 	@Before
     public void setUp() throws Exception {
+		mEventBus = new CountingEventBus();
+		mPlayItemBeanFactory = GWT.create(PlayItemBeanFactory.class);
 		mPlaylistController = new PlaylistController(mEventBus, mPlayItemBeanFactory);
     }
 	
 	@Test
-	public void onApplicationLoadEvent() {
+	public void onApplicationLoadEvent() throws Exception {
 		ApplicationConfig oConfig = new ApplicationConfig();
-		JSONArray oPlaylist = mock(JSONArray.class);
-		JSONObject oPlayItem1 = mock(JSONObject.class);
+		JSONArray oPlaylist = new JSONArray();
+		JSONObject oPlayItem1 = new JSONObject();
 		oPlayItem1.put("url", new JSONString("http://videos.tripfilms.com/360p/4BAE2BA4EE543703AF8BC2DF6550BB73.mp4"));
 		oPlaylist.set(0, oPlayItem1);
-		JSONObject oPlayItem2 = mock(JSONObject.class);
+		JSONObject oPlayItem2 = new JSONObject();
 		oPlayItem2.put("url", new JSONString("http://videos.tripfilms.com/480p/ECAA41D5E30E4BECC13FC1D3DA823750.mp4"));
 		oPlaylist.set(1, oPlayItem2);
+		System.out.println("Playlist = " + oPlaylist.size());
 		oConfig.setPlaylist(oPlaylist);
 		Application oApp = new Application(oConfig);
 		ApplicationLoadEvent.fire(mPlaylistController, oApp);
 		
 		ApplicationReadyEvent.fire(mPlaylistController);
-		assertEquals(1, mEventBus.getHandlerCount(PlaylistPlayEvent.getType()));
+		assertEquals(1, mEventBus.getFiredCount(PlaylistPlayEvent.getType()));
 		
 		PlayerPlayEndedEvent.fire(mPlaylistController);
-		assertEquals(2, mEventBus.getHandlerCount(PlaylistPlayEvent.getType()));
+		assertEquals(2, mEventBus.getFiredCount(PlaylistPlayEvent.getType()));
 		
 		// nothing more to play. No event should be triggered then
 		PlayerPlayEndedEvent.fire(mPlaylistController);
-		assertEquals(2, mEventBus.getHandlerCount(PlaylistPlayEvent.getType()));
+		assertEquals(2, mEventBus.getFiredCount(PlaylistPlayEvent.getType()));
 	}
 }
