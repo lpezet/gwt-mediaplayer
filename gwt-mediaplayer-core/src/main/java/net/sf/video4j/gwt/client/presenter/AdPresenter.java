00096e7d897660758aec3347c68adb5831666918
Luc Pezet <lpezet@gmail.com>
2013-11-05 22:39:16 +0000

    first try inserting ads (when available) in playlist.

--- gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/presenter/AdPresenter.java
+++ gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/presenter/AdPresenter.java
@@ -3,21 +3,36 @@ package net.sf.video4j.gwt.client.presenter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import net.sf.video4j.gwt.client.IConfigProvider;
 import net.sf.video4j.gwt.client.dispatch.AsyncCallbackImpl;
+import net.sf.video4j.gwt.client.event.ApplicationInitEvent;
+import net.sf.video4j.gwt.client.event.ApplicationInitEvent.ApplicationInitHandler;
+import net.sf.video4j.gwt.client.event.ApplicationLoadEvent;
+import net.sf.video4j.gwt.client.event.ApplicationLoadEvent.ApplicationLoadHandler;
+import net.sf.video4j.gwt.client.event.ApplicationReadyEvent;
+import net.sf.video4j.gwt.client.event.ApplicationReadyEvent.ApplicationReadyHandler;
 import net.sf.video4j.gwt.client.event.PlaylistPlayEvent;
 import net.sf.video4j.gwt.client.event.PlaylistPlayEvent.PlaylistPlayHandler;
+import net.sf.video4j.gwt.client.event.PluginReadyEvent;
+import net.sf.video4j.gwt.client.model.IApplication;
 import net.sf.video4j.gwt.client.model.IApplicationConfig;
 import net.sf.video4j.gwt.client.model.IPlugin;
 import net.sf.video4j.gwt.client.model.PlayerParameters;
+import net.sf.video4j.gwt.client.player.Media;
+import net.sf.video4j.gwt.client.player.MediaType;
+import net.sf.video4j.gwt.client.player.PlayItem;
+import net.sf.video4j.gwt.client.player.PlaylistNavigator;
 import net.sf.video4j.gwt.plugin.client.vast.dao.IAdService;
 import net.sf.video4j.gwt.plugin.shared.vast.Ad;
 import net.sf.video4j.gwt.plugin.shared.vast.AdRequestCallback;
+import net.sf.video4j.gwt.plugin.shared.vast.Creative;
 import net.sf.video4j.gwt.plugin.shared.vast.InLine;
+import net.sf.video4j.gwt.plugin.shared.vast.Linear;
+import net.sf.video4j.gwt.plugin.shared.vast.MediaFile;
 import net.sf.video4j.gwt.plugin.shared.vast.VAST;
 import net.sf.video4j.gwt.shared.FetchAdAction;
 import net.sf.video4j.gwt.shared.model.FetchAdResult;
 
+import com.google.gwt.json.client.JSONValue;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.dispatch.shared.DispatchAsync;
@@ -29,7 +44,12 @@ import fr.hd3d.html5.video.client.VideoSource.VideoType;
 /**
  * @author gumatias
  */
-public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements IPlugin, PlaylistPlayHandler {
+public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements 
+	IPlugin, 
+	PlaylistPlayHandler,
+	ApplicationLoadHandler,
+	ApplicationInitHandler, 
+	ApplicationReadyHandler {
 
     public interface AView extends View {
         void startPlayer(PlayerParameters pParams);
@@ -39,17 +59,24 @@ public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements I
     
 	private IAdService		mAdService;
 	private DispatchAsync	mDispatcher;
-	private IConfigProvider	mConfigProvider;
+	private IApplication 	mApplication;
+	
     
     @Inject
-	public AdPresenter(EventBus pEventBus, AView pView, IAdService pAdService, DispatchAsync pDispatcher, IConfigProvider pConfigProvider) {
+	public AdPresenter(EventBus pEventBus, AView pView, IAdService pAdService, DispatchAsync pDispatcher) {
         super(pEventBus, pView);
 		mAdService = pAdService;
 		mDispatcher = pDispatcher;
-		mConfigProvider = pConfigProvider;
-        addRegisteredHandler(PlaylistPlayEvent.getType(), this);
+		registerHandlers();
         mLogger.log(Level.INFO, "Creating Ad Presenter");
     }
+    
+    private void registerHandlers() {
+		addRegisteredHandler(ApplicationLoadEvent.getType(), this);
+		addRegisteredHandler(ApplicationReadyEvent.getType(), this);
+		addRegisteredHandler(ApplicationInitEvent.getType(), this);
+		addRegisteredHandler(PlaylistPlayEvent.getType(), this);
+	}
     
     @Override
     public String getPluginId() {
@@ -59,8 +86,41 @@ public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements I
     @Override
     protected void onBind() {
         super.onBind();
-		fetchAdsFromServlet();
-//      fetchAdsFromAJAX();
+		// fetchAdsFromServlet();
+        // fetchAdsFromAJAX();
+    }
+    
+    @Override
+    public void onApplicationInitEvent(ApplicationInitEvent pEvent) {
+    	mLogger.log(Level.FINE, "Received ApplicationInitEvent.");
+    	fetchAdsFromServlet();
+		mLogger.log(Level.FINE, "PluginReadyEvent fired.");
+    }
+    
+    @Override
+    public void onApplicationLoadEvent(ApplicationLoadEvent pEvent) {
+    	mLogger.log(Level.FINE, "Received ApplicationLoadEvent...");
+    	mApplication = pEvent.getApplication();
+		pEvent.getApplication().addPlugin(this);
+		IApplicationConfig oAppConfig = pEvent.getApplication().getConfig();
+		if (oAppConfig.getPlugins().isNull() != null) {
+			mLogger.log(Level.SEVERE, "No plugins found in configuration.");
+			return;
+		} else {
+			mLogger.log(Level.FINE, "Plugins : " + oAppConfig.getPlugins());
+		}
+		JSONValue oConfig = oAppConfig.getPlugins().get("net.sf.video4j.gwt.plugins.ads.liverail");
+		if (oConfig.isNull() != null) {
+			mLogger.log(Level.SEVERE, "Missing plugin \"net.sf.video4j.gwt.plugins.ads.liverail\" from configuration. No ads will be played.");
+			return;
+		}
+		//TODO: get LiveRail config and store them here
+    }
+    
+    @Override
+    public void onApplicationReadyEvent(ApplicationReadyEvent pEvent) {
+    	// TODO Auto-generated method stub
+    	
     }
 
     private void fetchAdsFromAJAX() {
@@ -101,9 +161,6 @@ public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements I
     private void fetchAdsFromServlet() {
         mLogger.log(Level.INFO, "Fetching Ads from servlet");
         FetchAdAction oAction = new FetchAdAction();
-
-		IApplicationConfig oConfig = mConfigProvider.getConfig();
-		mLogger.log(Level.INFO, "app config from config provider=" + oConfig);
 //		IAdBeanFactory oAdBeanFactory = GWT.create(IAdBeanFactory.class);
 //		BeanFactory<IAdBean, IAdBeanFactory> oFactory = new BeanFactory<IAdBean, IAdBeanFactory>(IAdBean.class, oAdBeanFactory);
 //		IAdBean oAdBean = oFactory.makeFrom(oConfig.getAd());
@@ -114,9 +171,57 @@ public class AdPresenter extends PresenterWidget<AdPresenter.AView> implements I
 
             @Override
             public void onSuccess(FetchAdResult pResult) {
-                mLogger.log(Level.INFO, "Got results=" + pResult);
+                
+                if (pResult == null || pResult.getVAST().getAds() == null || pResult.getVAST().getAds().isEmpty()) {
+                	mLogger.log(Level.INFO, "Got no Ads.");
+                } else {
+                	mLogger.log(Level.INFO, "Got " + pResult.getVAST().getAds().size() + " Ads.");
+                	
+                	MediaFile oFirstMP4MediaFile = getFirstMP4MediaFile(pResult.getVAST());
+                	if (oFirstMP4MediaFile != null) {
+                		Media oMedia = new Media();
+						oMedia.setURI(oFirstMP4MediaFile.getURI().asString());
+						oMedia.setAd(true);
+						oMedia.setType(MediaType.Video);
+						PlaylistNavigator oNav = new PlaylistNavigator(mApplication.getPlaylist());
+						// Doing pre-roll:
+						PlayItem oFirstPlayItem = oNav.peek();
+						Media oParent = oFirstPlayItem.getMedia();
+						mApplication.getPlaylist().addChild(oMedia, oParent, 0);
+                	}
+	                
+                }
+                PluginReadyEvent.fire(AdPresenter.this, AdPresenter.this);
             }
             
+            private MediaFile getFirstMP4MediaFile(VAST pVAST) {
+            	for (Ad oAd : pVAST.getAds()) {
+                	if (oAd instanceof InLine) {
+                		InLine oInLine = (InLine) oAd;
+                		if (oInLine.getCreatives() == null || oInLine.getCreatives().isEmpty()) {
+                			mLogger.log(Level.SEVERE, "Got no creatives. Something wrong?");
+                		} else {
+                			for (Creative c : oInLine.getCreatives()) {
+                				if (c instanceof Linear) {
+                					Linear oLinearAd = (Linear) c;
+                					if (oLinearAd.getMediaFiles() == null || oLinearAd.getMediaFiles().isEmpty()) {
+                						mLogger.log(Level.SEVERE, "Got no media files from Linear Ad. Something wrong?");
+                					} else {
+                						for (MediaFile oMediaFile : oLinearAd.getMediaFiles()) {
+                							if ("video/mp4".equalsIgnoreCase(oMediaFile.getType())) {
+                								// use the first one as a test right now
+                								mLogger.log(Level.INFO, "Using media file: type=" + oMediaFile.getType() + ", uri=" + oMediaFile.getURI().asString() + ", bitrate=" + oMediaFile.getBitrate() + ", width=" + oMediaFile.getWidth() + ", height=" + oMediaFile.getHeight());
+                								return oMediaFile;
+                							}
+                						}
+                					}
+                				}
+                			}
+                		}
+                	}
+                }
+            	return null;
+            }
         });
     }
 
