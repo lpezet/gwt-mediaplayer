d2d72db246264de13f2a2025e257d716467212eb
Gustavo Matias <gustavo@matias.com>
2013-11-05 04:38:38 +0000

    (work in progress) trying to grab plugins from JSON object

--- gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/util/PluginsBeanFactory.java
+++ gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/util/PluginsBeanFactory.java
@@ -1,6 +1,6 @@
 package net.sf.video4j.gwt.client.util;
 
-import net.sf.video4j.gwt.client.model.PluginsBean;
+import java.util.Map;
 
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
@@ -9,5 +9,5 @@ import com.google.web.bindery.autobean.shared.AutoBeanFactory;
  * @author Gustavo Matias
  */
 public interface PluginsBeanFactory extends AutoBeanFactory {
-	AutoBean<PluginsBean> makeBean();
+	AutoBean<Map<String, Map<String, String>>> makeBean();
 }
