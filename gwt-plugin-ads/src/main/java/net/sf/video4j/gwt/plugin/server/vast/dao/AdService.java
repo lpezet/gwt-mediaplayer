5db779dbbd1719dc67305b539ecc430eb1cdf852
Luc Pezet <lpezet@gmail.com>
2013-11-06 07:24:22 +0000

    implemented using new server-side parser

--- gwt-plugin-ads/src/main/java/net/sf/video4j/gwt/plugin/server/vast/dao/AdService.java
+++ gwt-plugin-ads/src/main/java/net/sf/video4j/gwt/plugin/server/vast/dao/AdService.java
@@ -1,17 +1,15 @@
 package net.sf.video4j.gwt.plugin.server.vast.dao;
 
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import net.sf.video4j.gwt.plugin.shared.vast.VAST;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
 
 /**
  * @author gumatias
@@ -22,21 +20,27 @@ public class AdService implements IAdService {
 
 	@Override
 	public VAST fetchAds(String pURL) {
-		Document oDoc = getXMLDocument(pURL);
-		mLogger.info("xml node=" + oDoc.getFirstChild().getNodeName());
+		try {
+			String oXML = getContent(pURL);
+			//mLogger.info("xml node=" + oDoc.getFirstChild().getNodeName());
+			VASTParser oParser = new VASTParser();
+			return oParser.parse(oXML);
+		}  catch (Throwable t) {
+			mLogger.error("Error getting VAST.", t);
+		}
 		return new VAST();
 	}
 
-	private Document getXMLDocument(String pURL) {
+	private String getContent(String pURL) {
 		try {
 			URL oURL = new URL(pURL);
 			HttpURLConnection pConnection = (HttpURLConnection) oURL.openConnection();
 			pConnection.setRequestMethod("GET");
 			pConnection.setRequestProperty("Accept", "application/xml");
 			InputStream oStream = pConnection.getInputStream();
-			DocumentBuilderFactory oBDF = DocumentBuilderFactory.newInstance();
-			DocumentBuilder oDB = oBDF.newDocumentBuilder();
-			return oDB.parse(oStream);
+			StringWriter oSW = new StringWriter();
+			IOUtils.copy(oStream, oSW);
+			return oSW.toString();
 		} catch (Exception e) {
 			mLogger.error("Error while fetching ad xml document", e);
 		}
