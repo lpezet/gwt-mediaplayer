661889eb79f78428584d7a0d15ef76e0dd6460b6
Luc Pezet <lpezet@gmail.com>
2013-11-07 23:54:02 +0000

    liverail config setup (for testing purposes for now)

--- gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/model/IAdBean.java
+++ gwt-mediaplayer-core/src/main/java/net/sf/video4j/gwt/client/model/IAdBean.java
@@ -11,4 +11,45 @@ public interface IAdBean {
 	String getURL();
 	@PropertyName("url")
 	void setURL(String pURL);
+	
+	@PropertyName("lr_publisher_id")
+	String getPublisherId();
+	@PropertyName("lr_publisher_id")
+	void setPublisherId(String pId);
+	
+	@PropertyName("lr_video_position")
+	String getVideoPosition();
+	@PropertyName("lr_video_position")
+	void setVideoPosition(String pPos);
+	
+	@PropertyName("lr_ad_unit")
+	String getAdUnit();
+	@PropertyName("lr_ad_unit")
+	void setAdUnit(String pValue);
+	
+	@PropertyName("lr_ad_message")
+	String getAdMessage();
+	@PropertyName("lr_ad_message")
+	void setAdMessage(String pMessage);
+	
+	@PropertyName("lr_verticals")
+	String getVerticals();
+	@PropertyName("lr_verticals")
+	void setVerticals(String pVerticals);
+	
+	@PropertyName("lr_video_id")
+	String getVideoId();
+	@PropertyName("lr_video_id")
+	void setVideoId(String pId);
+	
+	@PropertyName("lr_autoplay")
+	boolean getAutoplay();
+	@PropertyName("lr_autoplay")
+	void setAutoplay(boolean pAutoplay);
+	
+	@PropertyName("lr_tags")
+	String getTags();
+	@PropertyName("lr_tags")
+	void setTags(String pTag);
+	
 }
